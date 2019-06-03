718a652d1bd01522f1388ec461cfdaeb56fe7e87
Ryan Wirth <ryan@ryanwirth.ca>
2019-06-03 07:33:17 -0700

    Add scrubber hover effect

--- src/components/music-controls/MusicControlsScrubber.vue
+++ src/components/music-controls/MusicControlsScrubber.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="music-controls-scrubber">
-    <div class="scrubber-track cb-primary-variant">
-      <div class="scrubber-track__fill cb-accent"></div>
+    <div class="track cb-primary-variant">
+      <div class="fill cb-accent"></div>
     </div>
-    <div class="scrubber-handle">
-      <div class="scrubber-handle__inner"></div>
+    <div class="handle">
+      <div class="inner"></div>
     </div>
   </div>
 </template>
@@ -15,43 +15,41 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .music-controls-scrubber {
   position: relative;
   width: 100%;
+
+  .track {
+    overflow: hidden;
+    width: 100%;
+    height: 4px;
+    border-radius: 2px;
+
+    .fill {
+      width: 75%;
+      height: 100%;
+    }
+  }
+
+  .handle {
+    position: absolute;
+    left: 75%;
+    margin: -26px 0px 0px -24px;
+    padding: 16px;
+    cursor: pointer;
+
+    .inner {
+      width: 16px;
+      height: 16px;
+      background-color: #FFFFFF;
+      border-radius: 8px;
+      box-shadow: 0px 1px 4px #545454;
+    }
+
+    &:hover .inner {
+      box-shadow: 0px 1px 8px #121212;
+    }
+  }
 }
-
-.scrubber-track {
-  overflow: hidden;
-
-  width: 100%;
-  height: 4px;
-
-  border-radius: 2px;
-}
-
-.scrubber-track__fill {
-  width: 75%;
-  height: 100%;
-}
-
-.scrubber-handle {
-  position: absolute;
-  left: 75%;
-
-  margin: -26px 0px 0px -24px;
-  padding: 16px;
-
-  cursor: pointer;
-}
-
-.scrubber-handle__inner {
-  width: 16px;
-  height: 16px;
-  
-  background-color: #FFFFFF;
-  border-radius: 8px;
-  box-shadow: 0px 1px 4px #545454;
-}
-
 </style>
