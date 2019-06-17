1841fb5dc87c5c5e9a899c28be305d129860071c
Ryan Wirth <ryan@ryanwirth.ca>
2019-06-16 20:58:38 -0700

    Create Sources sidebar section

--- packages/machete-app/src/components/sidebar/Sidebar.vue
+++ packages/machete-app/src/components/sidebar/Sidebar.vue
@@ -2,18 +2,21 @@
   <div class="sidebar">
     <Library/>
     <Playlists/>
+    <Sources/>
   </div>
 </template>
 
 <script>
 import Library from './SidebarSectionLibrary.vue'
 import Playlists from './SidebarSectionPlaylists.vue'
+import Sources from './SidebarSectionSources.vue'
 
 export default {
   name: 'Sidebar',
   components: {
     Library,
-    Playlists
+    Playlists,
+    Sources
   }
 }
 </script>
---
+++ packages/machete-app/src/components/sidebar/SidebarSectionSources.vue
@@ -0,0 +1,23 @@
+<template>
+  <Section name="Sources">
+    <Button label="YouTube">
+      <IconYouTube/>
+    </Button>
+  </Section>
+</template>
+
+<script>
+import Button from './SidebarSectionButton.vue'
+import Section from './SidebarSection.vue'
+
+import IconYouTube from '@/components/icons/IconYouTube.vue'
+
+export default {
+  name: 'SidebarSectionSources',
+  components: {
+    Button,
+    IconYouTube,
+    Section
+  }
+}
+</script>
