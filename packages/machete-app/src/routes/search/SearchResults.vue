4b9b0135f5162c3ea82f26a0f0ee435b7dfd22f6
Ryan Wirth <ryan@ryanwirth.ca>
2019-06-23 00:33:23 -0700

    Create Grid component

--- packages/machete-app/src/routes/search/SearchResults.vue
+++ packages/machete-app/src/routes/search/SearchResults.vue
@@ -1,33 +1,24 @@
 <template>
-  <div class="search-results">
-    <Track
-      v-for="result in results"
-      :key="result.id"
-      :data="result"
-    />
-  </div>
+  <Grid
+    v-slot="{ item }"
+    :items="results"
+  >
+    <Track :data="item"/>
+  </Grid>
 </template>
 
 <script>
+import Grid from '@/components/Grid.vue'
 import Track from '@/components/tracks/TrackMedium.vue'
 
 export default {
-  name: 'Search',
+  name: 'SearchResults',
   components: {
+    Grid,
     Track
   },
   props: {
-    results: {
-      type: Array,
-      required: true
-    }
+    results: Array
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.search-results {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
-}
-</style>
