eae26561bbf2c1bd06070cbcb5f682323fc8f914
Ryan Wirth <ryan@ryanwirth.ca>
2019-06-23 19:52:45 -0700

    Remove navigation components

--- packages/machete-app/src/components/top-bar/TopBar.vue
+++ packages/machete-app/src/components/top-bar/TopBar.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="top-bar color-background-variant-bg color-gutter-border">
-    <Navigation/>
     <Search/>
   </div>
 </template>
 
 <script>
-import Navigation from './TopBarNavigation.vue'
 import Search from './TopBarSearch.vue'
 
 export default {
   name: 'TopBar',
   components: {
-    Navigation,
     Search
   }
 }
