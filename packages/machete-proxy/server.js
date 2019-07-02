61b98a274a8597e2982434c6c1d2c4ecb88ce17c
Ryan Wirth <ryan@ryanwirth.ca>
2019-07-01 18:55:08 -0700

    Add ryanwirth.ca to machete-proxy whitelist

--- packages/machete-proxy/server.js
+++ packages/machete-proxy/server.js
@@ -3,9 +3,14 @@ const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 9000;
 
+const whitelist = [
+  'http://localhost:8080',
+  'https://ryanwirth.ca'
+];
+
 const corsProxy = require('cors-anywhere');
 corsProxy.createServer({
-  originWhitelist: ['http://localhost:8080'],
+  originWhitelist: whitelist,
   requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['origin'],
   setHeaders: {
