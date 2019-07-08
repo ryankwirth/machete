9f1fa3708f5e3b50835cf576716ba2bd9fc8f39e
Ryan Wirth <ryan@ryanwirth.ca>
2019-07-07 21:20:41 -0700

    Remove duplicate songs/playlists in history

--- packages/machete-app/src/store/history/mutations.js
+++ packages/machete-app/src/store/history/mutations.js
@@ -1,7 +1,13 @@
+function insertIntoArray(array, item) {
+  const newArray = array.filter((element) => element.uri !== item.uri)
+  newArray.unshift(item)
+  return newArray
+}
+
 export const listenToPlaylist = (state, playlist) => {
-  state.playlists.unshift(playlist)
+  state.playlists = insertIntoArray(state.playlists, playlist)
 }
 
 export const listenToSong = (state, song) => {
-  state.songs.unshift(song)
+  state.songs = insertIntoArray(state.songs, song)
 }
